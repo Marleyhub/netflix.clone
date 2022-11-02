38ec7fcf890e35d8afd4efc9cdebc2866a1984e2
Gabriel Marley <cardoso_eng@outlook.com>
2022-11-02 18:40:23 -0300

    featuredMovie changes

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,6 +3,11 @@ import './FeaturedMovie.css'
 
 
 export default ({item}) => {
+    let firstDate = new Date(item.first_air_date)
+    let genres =[]
+    for(let i in item.genres){
+        genres.push(item.genres[i].name)
+    }
     return(
            
             <section className='featured' style={{
@@ -10,8 +15,21 @@ export default ({item}) => {
                 backgroudPosition: 'center',
                 backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
             }}> 
-            <div>
-                {item.original_name}
+            <div className='featured--vertical'>
+                <div className='featured--horizontal'>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+                <div className='featured--name'>{item.original_name}</div>
+                <div className='featured--info'></div>
+                <div className='featured--points'>{item.vote_avarage} pontos</div>
+                <div className='featured--year'>{firstDate.getFullYear()}</div>
+                <div className='featured--seasons'>{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                <div className='featured--description'>{item.overview}</div>
+                <div className='featured--buttons'>
+                    <a href={`watch/${item.Id}`}> ► Watch </a>
+                    <a href={`/list/add/${item.id}`}> + Watchlist</a>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+                </div>
+                <div className='featured--genres'><strong>Gênero: </strong>{genres.join(', ')}</div>
+
+                </div> 
             </div>
             </section>
         
