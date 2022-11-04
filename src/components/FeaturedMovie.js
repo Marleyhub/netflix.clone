c817c80e798cdbfc9291974fac70d869bcfec4db
Gabriel Marley <cardoso_eng@outlook.com>
2022-11-04 15:02:21 -0300

    styling featured movie

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -7,7 +7,9 @@ export default ({item}) => {
     let genres =[]
     for(let i in item.genres){
         genres.push(item.genres[i].name)
+        console.log(item)
     }
+
     return(
            
             <section className='featured' style={{
@@ -18,14 +20,15 @@ export default ({item}) => {
             <div className='featured--vertical'>
                 <div className='featured--horizontal'>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
                 <div className='featured--name'>{item.original_name}</div>
-                <div className='featured--info'></div>
-                <div className='featured--points'>{item.vote_avarage} pontos</div>
+                <div className='featured--info'>
+                <div className='featured--points'>{item.vote_average} pontos</div>
                 <div className='featured--year'>{firstDate.getFullYear()}</div>
                 <div className='featured--seasons'>{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                </div>
                 <div className='featured--description'>{item.overview}</div>
                 <div className='featured--buttons'>
-                    <a href={`watch/${item.Id}`}> ► Watch </a>
-                    <a href={`/list/add/${item.id}`}> + Watchlist</a>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+                    <a href={`watch/${item.Id}`} className='featured--watchbutton'> ► Watch </a>
+                    <a href={`/list/add/${item.id}`} className='featured--mylistbutton'> + Watchlist</a>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
                 </div>
                 <div className='featured--genres'><strong>Gênero: </strong>{genres.join(', ')}</div>
 
