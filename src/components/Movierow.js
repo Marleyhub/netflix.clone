ce9fd9804f266d876480b0caa9196783a6e3a5b3
Gabriel Marley <cardoso_eng@outlook.com>
2022-11-07 21:48:24 -0300

    fail: can't do all ease transition

--- src/components/Movierow.js
+++ src/components/Movierow.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 import './Movierow.css'
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore'
 import NavigateNextIcon from '@mui/icons-material/NavigateNext'
-import { width } from '@mui/system'
+
 
 // eslint-disable-next-line import/no-anonymous-default-export
  export default ({title, items}) => {
-   const [scrollX, setScrollX] = useState(-400)
+   const [scrollX, setScrollX] = useState(0)
 
     const handleLeft = () => {
-      let x = scrollX + 150 
+      let x = scrollX + Math.round(window.innerWidth / 2)
       if(x > 0){
         x = 0
       }
@@ -17,7 +17,12 @@ import { width } from '@mui/system'
 
     }
     const handleRigth = () => {
-
+      let x = scrollX - Math.round(window.innerWidth / 2)
+      let listW = items.results.length * 150
+      if((window.innerWidth - listW) > x){
+        x = (window.innerWidth - listW) - 60
+      }
+      setScrollX(x)
     }
     return(
     <div className='movieRow'>
@@ -29,18 +34,19 @@ import { width } from '@mui/system'
       <NavigateNextIcon style ={{fontSize: 50}}/>
      </div> 
     <div className='movieRow--listarea'>
-      <div className="movieRow--list" style={{
-            marginLeft: scrollX,
-            width: items.results.length * 150
-        }}>
-        {items.results.length > 0 && items.results.map((item, key)=>(
-            <div key={key} className="movieRow--item">
-            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
-            </div>
-        ))}
+        <div className= 'movieRow--list' style={{
+              marginLeft: scrollX,
+              width: items.results.length * 150
+              
+            }}>
+              {items.results.length > 0 && items.results.map((item, key)=>(
+              <div key={key} className="movieRow--item">
+                <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+              </div>
+           ))}
         </div>
     </div>
-    </div>
+</div>
     )}
     
 
